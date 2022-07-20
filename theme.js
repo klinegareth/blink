9fa8cdd683a55fd68d3e2beaad2326e22ee1f3aa
Kline Gareth <klinegareth@gmail.com>
2022-07-20 18:14:10 +0000

    Changed theme colors

--- theme.js
+++ theme.js
@@ -1,4 +1,26 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#da4939", "#519f50", "#ffd24a", "#6d9cbe", "#d0d0ff", "#6e9cbe", "#ffffff", "#323232", "#ff7b6b", "#83d182", "#ffff7c", "#9fcef0", "#ffffff", "#a0cef0", "#ffffff"]);
-t.prefs_.set('foreground-color', "#e6e1dc");
-t.prefs_.set('background-color', "#2b2b2b");
-t.prefs_.set('cursor-color', 'rgba(255,255,255,0.5)');
+black       = '#000000';
+red         = '#cc241d'; // red
+green       = '#b8bb26'; // green
+yellow      = '#d79921'; // yellow
+blue        = '#458588'; // blue
+magenta     = '#d3869b'; // pink
+cyan        = '#689d6a'; // cyan
+white       = '#a89984'; // light gray
+lightBlack  = '#928374'; // medium gray
+lightRed    = '#fb4934'; // red
+lightGreen  = '#b8bb26'; // green
+lightYellow = '#d65d0e'; // yellow
+lightBlue   = '#83a598'; // blue
+lightMagenta= '#d3869b'; // pink
+lightCyan   = '#689d6a'; // cyan
+lightWhite  = '#ebdbb2'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#000000');
+t.prefs_.set('background-color', white);
